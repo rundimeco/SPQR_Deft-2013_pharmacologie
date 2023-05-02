bd3b4629c3f29775d16726434d91aec0ef4912d9
toufikB <toufik.boubehziz@gmail.com>
2023-05-02 11:50:55 +0200

    similarity test - 1st code

--- team_TB/similarityFcts.py
+++ team_TB/similarityFcts.py
@@ -2,6 +2,7 @@ from fuzzywuzzy import fuzz
 import pandas as pd 
 import spacy
 nlp = spacy.load("fr_core_news_lg")
+nlpd = spacy.load("fr_core_news_md")
 from fuzzywuzzy import fuzz
 import json 
 
@@ -18,15 +19,39 @@ def ComMembList(keywordQ, keywordR, tol=90):
         return False,rate
     
 # Search for Medkeywords in the DB 
-def getRepDB(keywordQ):
-    indx = 0
+def getRepDB(keywordQ,nwQstp):
+    indxA = 0
+    indxB = 0
     rateRef = 0
-    corpusA = pd.read_csv("./output/CorpusAnw.csv",delimiter=";")
-    keywordDBd = corpusA["ListMed"]
-    for ik in range(10): #len(keywordDBd)
-        keywordDB = keywordDBd[ik].replace(","," ")
-        keywordDB = keywordDB.split()
-        _, rate = ComMembList(keywordQ, keywordDB, tol=90)
-        if rate > rateRef:
-            indx = ik
-    return indx
+    rateA = 0
+    rateB = 0
+    with open('output/corpusMerck.json', 'r') as f:
+        corpusB = json.load(f)
+    with open('output/CorpusMS.json', 'r') as fs:
+        corpusA = json.load(fs)
+    keywordA = [item['keywords'] for item in corpusA]
+    keywordB = [item['keywords'] for item in corpusB]
+    for ikA in range(len(keywordA)):
+        _, rateA = ComMembList(keywordQ, keywordA, tol=90)
+        if rateA > rateRef:
+            indxA = ikA
+    for ikB in range(len(keywordB)):
+        _, rateB = ComMembList(keywordQ, keywordB, tol=90)
+        if rateB > rateRef:
+            indxB = ikB
+    
+    if indxA !=0:
+        rateA = similTestStcs(nwQstp,corpusA[indxA])
+    if indxB !=0:
+        rateB = similTestStcs(nwQstp,corpusB[indxB])
+    
+    return max(rateA,rateB)
+
+def similTestStcs(qst,Stc):
+
+    vectQst = nlpd(qst).vector
+    vectStc = nlpd(Stc).vector
+
+    similarity = vectQst.dot(vectStc) / (vectQst.norm() * vectStc.norm())
+
+    return similarity
