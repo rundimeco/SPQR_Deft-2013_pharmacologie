488028e2078facd6b26f2a1eb5e418ea335f98d4
Behziz <toufik.boubehziz@gmail.com>
2023-05-06 14:48:57 +0200

    2nd approach - needs some improvements

--- team_TB/similarityFcts.py
+++ team_TB/similarityFcts.py
@@ -1,63 +1,70 @@
 from fuzzywuzzy import fuzz
 import pandas as pd 
 import spacy
-nlp = spacy.load("fr_core_news_lg")
+# nlp = spacy.load("fr_core_news_lg")
 nlpd = spacy.load("fr_core_news_md")
-from fuzzywuzzy import fuzz
+
 import json 
 
-
-def writeJson(path,data):
-    with open(path,"w",encoding='utf-8') as f:
-        json.dump(data,f,indent=4,ensure_ascii=False)
-
-        
-def ComMembList(keywordQ, keywordR, tol=90):
-    rate = 0
-    for elt1 in keywordQ:
-        for elt2 in keywordR:
-            score = fuzz.token_sort_ratio(elt1, elt2)
-            if score >= tol:
-                rate = rate +1
-    if rate > 1:
-        return True, rate 
-    else:
-        return False,rate
-    
-# Search for Medkeywords in the DB 
-def getRepDB(keywordQ,nwQstp):
-    indxA = 0
-    indxB = 0
-    rateRef = 0
-    rateA = 0
-    rateB = 0
-    with open('output/corpusMerck.json', 'r') as f:
-        corpusB = json.load(f)
-    with open('output/CorpusMS.json', 'r') as fs:
-        corpusA = json.load(fs)
-    keywordA = [item['keywords'] for item in corpusA]
-    keywordB = [item['keywords'] for item in corpusB]
-    for ikA in range(len(keywordA)):
-        _, rateA = ComMembList(keywordQ, keywordA, tol=90)
-        if rateA > rateRef:
-            indxA = ikA
-    for ikB in range(len(keywordB)):
-        _, rateB = ComMembList(keywordQ, keywordB, tol=90)
-        if rateB > rateRef:
-            indxB = ikB
-    
-    if indxA !=0:
-        rateA = similTestStcs(nwQstp,corpusA[indxA])
-    if indxB !=0:
-        rateB = similTestStcs(nwQstp,corpusB[indxB])
-    
-    return max(rateA,rateB)
+from collections import Counter
+import re
+import numpy as np
 
 def similTestStcs(qst,Stc):
 
     vectQst = nlpd(qst).vector
     vectStc = nlpd(Stc).vector
-
-    similarity = vectQst.dot(vectStc) / (vectQst.norm() * vectStc.norm())
+    similarity = vectQst.dot(vectStc) / (np.linalg.norm(vectQst) * np.linalg.norm(vectStc))
+    # similarity = vectQst.dot(vectStc) / (vectQst.norm() * vectStc.norm())
 
     return similarity
+
+def count_words(keywordQ, KwCorpus):
+    word_counter = Counter()
+    cpt = 0
+    for word in keywordQ:
+        word_singular = re.sub(r's$', '', word) 
+        word_plural = word_singular + 's' 
+        word_counter[word] = 0
+        word_counter[word_singular] = 0
+        word_counter[word_plural] = 0
+
+    for text in KwCorpus:
+        for word in word_counter:
+            if re.search(r'\b{}\b'.format(word), text, re.IGNORECASE):
+                cpt += 1
+                # word_counter[word] += 1
+    return cpt
+
+# Search for Medkeywords in the DB 
+def getRepDB(keywordQ,keywordQP,corpus,nwQstp):
+    taux_simStc = 0
+    indxStc = 0
+    taux_sim = 0
+    taux_simP = 0
+    rateA = 0
+    rateAP = 0
+    keywordA = [item['keywords'] for item in corpus]
+
+    for ikA in range(len(keywordA)):
+        rateA = count_words(keywordQ, keywordA[ikA])
+        if rateA >0:
+            rateAP = count_words(keywordQP, keywordA[ikA])
+            if rateA > taux_sim:
+                if rateAP > taux_simP:
+                    indxStc = ikA
+                    taux_sim = rateA
+                    taux_simP = rateAP
+                    # print(indxStc)
+    
+    # if indxStc !=0:
+    if taux_simP !=0:
+        # print(corpus[indxStc]['sentence'])
+        taux_simStc = similTestStcs(nwQstp,corpus[indxStc]['sentence'])
+        # print(taux_simStc)
+    return taux_simStc
+
+
+def writeJson(path,data):
+    with open(path,"w",encoding='utf-8') as f:
+        json.dump(data,f,indent=4,ensure_ascii=False)
